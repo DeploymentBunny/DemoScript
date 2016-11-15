6fe493c0831d6ab39947ec9173c7723a5bf809bf
Mikael Nystrom <deploymentbunny@outlook.com>
2016-11-15 20:36:52 +0100

    Adding File

---
+++ 2016-11-15 - TechDays/ADHealthCheck.ps1
@@ -0,0 +1,26 @@
+$getForest = [system.directoryservices.activedirectory.Forest]::GetCurrentForest()
+$DCServers = $getForest.domains | ForEach-Object {$_.DomainControllers} | ForEach-Object {$_.Name} 
+
+foreach ($DCServer in $DCServers){
+    Write-Host "Checking netaccess to $DCServer" -ForegroundColor Green
+    Test-Connection -ComputerName $DCServer
+
+    Write-Host "Checking Services that should be running on $DCServer" -ForegroundColor Green
+    Invoke-Command -ComputerName $DCServer -ScriptBlock {
+        $Services = Get-Service
+        Foreach($Service in $Services | Where-Object -Property StartType -EQ Automatic){
+            $Service | Where-Object -Property Status -NE -Value Running
+            }
+    }
+
+    Write-Host "Running DCDiag on $DCServer" -ForegroundColor Green
+    Invoke-Command -ComputerName $DCServer -ScriptBlock {
+        dcdiag.exe /test:netlogons /Q
+        dcdiag.exe /test:Services /Q
+        dcdiag.exe /test:Advertising /Q
+        dcdiag.exe /test:FSMOCheck /Q
+    }
+
+    Write-Host "Checking access to SYSVOL on $DCServer" -ForegroundColor Green
+    Test-Path -Path \\$DCServer\sysvol
+}
